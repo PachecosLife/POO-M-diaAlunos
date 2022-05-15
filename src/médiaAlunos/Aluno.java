f7b4af786e96772e363dccb7446209c54057656c
i5 <i5@Pachecos>
2022-05-15 02:13:59 -0300

    Média de Alunos com POO

--- src/médiaAlunos/Aluno.java
+++ src/médiaAlunos/Aluno.java
@@ -2,58 +2,52 @@ package médiaAlunos;
 
 import javax.swing.JOptionPane;
 
-public class Aluno {
-			
+public class Aluno { 
+			 // CLASSE ALUNO
 	String nome = JOptionPane.showInputDialog("Digite seu primeiro nome");
 	String nasc = JOptionPane.showInputDialog("Olá " + nome + ". Digite o ANO em que nasceu");
 	String nota1 = JOptionPane.showInputDialog("Digite a 1° Nota");
 	String nota2 = JOptionPane.showInputDialog("Digite a 2° Nota");
+	
+	 	// MÉTODO QUE CONVERTE STRING EM INTEIROS E DOUBLE
 	int anoNasc = Integer.parseInt(nasc);
 	double n1 = Double.parseDouble(nota1);
 	double n2 = Double.parseDouble(nota2);
 	double media = (n1 + n2) / 2;
 		
-	
+		// MÉTODOS SETTERS & GETTERS
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public int getAnoNasc() {
 		return anoNasc;
 	}
-
 	public void setAnoNasc(int anoNasc) {
 		this.anoNasc = anoNasc;
 	}
-
 	public double getN1() {
 		return n1;
 	}
-
 	public void setN1(double n1) {
 		this.n1 = n1;
 	}
-
 	public double getN2() {
 		return n2;
 	}
-
 	public void setN2(double n2) {
 		this.n2 = n2;
 	}
-
 	public double getMedia() {
 		return media;
 	}
-
 	public void setMedia(double media) {
 		this.media = media;
 	}
-
+	
+		// MÉTODO QUE AVALIA NOTA E DEFINE APROVADO OU REPROVADO
 	public String condicao () {
 		if (media > 5) {
 			return ("Aluno APROVADO, PARABÉNS");
--- src/médiaAlunos/Principal.java
+++ src/médiaAlunos/Principal.java
@@ -4,14 +4,17 @@ public class Principal extends Aluno{
 
 	public static void main(String[] args) {
 		
+		 	// CLASSE PRINCIPAL QUE EXECUTA O OBJETO ALUNO 
 		System.out.println("========== SEJA BEM VINDO ==========");
 		
+		//PRIMEIRO ALUNO
 		Aluno a1 = new Aluno();
 		System.out.println(a1.toString());
 		System.out.println(a1.condicao());
 
 		System.out.println("===================");
 		
+		//SEGUNDO ALUNO
 		Aluno a2 = new Aluno();
 		System.out.println(a2.toString());
 		System.out.println(a2.condicao());
